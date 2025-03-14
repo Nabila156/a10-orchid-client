28bf677738be20cdd172fe98ffdd4d56d962caa2
Nabila <nowshinnabila24@gmail.com>
2025-03-14 13:37:04 +0600

    firebase deployment done

--- src/AddMovie/AddMovie.jsx
+++ src/AddMovie/AddMovie.jsx
@@ -1,60 +1,80 @@
 
 
 const AddMovie = () => {
+
+    const handleAddMovie = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const poster = form.poster.value;
+        const genre = form.genre.value;
+        const duration = form.duration.value;
+        const year= form.year.value;
+        const rating = form.rating.value;
+        const summary = form.summary.value;
+
+        const newMovie = {poster,genre,duration,year,rating,summary};
+        console.log(newMovie)
+
+        // send data to the server
+        fetch('https://orchid-server-gilt.vercel.app/',{
+            
+        })
+    }
     return (
         <div>
             <p className="text-4xl text-center py-8 font-bold">Add Your Movie</p>
             <div className="hero px-10">
                 <div className="card w-full bg-base-100 shadow-2xl">
-                    <div className="card-body">
+                    <form onSubmit={handleAddMovie} className="card-body">
                         <fieldset className="fieldset">
 
                             <div className="flex gap-10">
                                 <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Movie Poster</label>
-                                    <input type="url" className="input w-full" placeholder="Movie Poster url" />
+                                    <input type="url" name="poster" className="input w-full" placeholder="Movie Poster url" />
                                 </div>
-                                <div className="flex flex-col w-1/2"> 
+                                <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Movie Title</label>
-                                    <input type="text" className="input w-full" placeholder="Movie Title" />
+                                    <input type="text" name="title" className="input w-full" placeholder="Movie Title" />
                                 </div>
                             </div>
 
                             <div className="flex gap-10">
                                 <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Genre</label>
-                                    <input type="text" className="input w-full" placeholder="Genre" />
+                                    <input type="text" name="genre" className="input w-full" placeholder="Genre" />
                                 </div>
-                                <div className="flex flex-col w-1/2"> 
+                                <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Duration</label>
-                                    <input type="text" className="input w-full" placeholder="Duration" />
+                                    <input type="text" name="duration" className="input w-full" placeholder="Duration" />
                                 </div>
                             </div>
 
                             <div className="flex gap-10">
                                 <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Release Year</label>
-                                    <input type="text" className="input w-full" placeholder="Release Year" />
+                                    <input type="text" name="year" className="input w-full" placeholder="Release Year" />
                                 </div>
-                                <div className="flex flex-col w-1/2"> 
+                                <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Rating</label>
-                                    <input type="text" className="input w-full" placeholder="Rating" />
+                                    <input type="text" name="rating" className="input w-full" placeholder="Rating" />
                                 </div>
                             </div>
 
                             <div className="flex gap-10">
-                                <div className="flex flex-col w-1/2"> 
+                                <div className="flex flex-col w-1/2">
                                     <label className="fieldset-label hidden md:block">Summary</label>
-                                    <input type="text" className="input w-full" placeholder="Summary" />
+                                    <input type="text" name="summary" className="input w-full" placeholder="Summary" />
                                 </div>
                                 <div className="flex flex-col w-1/2">
 
                                 </div>
                             </div>
-                            
+
                             <button className="btn btn-neutral mt-4">Add Movie</button>
                         </fieldset>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
