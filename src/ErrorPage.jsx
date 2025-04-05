9bf85eea3d11e4fc629251e7a69f9024b04ce68d
Nabila <nowshinnabila24@gmail.com>
2025-04-05 08:12:14 +0600

    errorpage new design done

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -2,9 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className='text-center my-20'>
-            <h1 className="text-4xl font-bold text-red-500">Oops! Something went wrong.</h1>
-            <NavLink to={'/'} className='btn text-white font-bold text-2xl mt-10 bg-gradient-to-r from-red-800 to-red-300'>Go Back to Home</NavLink>
+        <div className='flex gap-10 min-h-screen bg-slate-300'>
+            <div className='min-w-[60%] w-full'>
+                <img className='h-full' src="https://i.ibb.co.com/9HQ02g2Y/eighteen-div02-img01-popcorn-error.jpg" alt="error" />
+            </div>
+            <div className='my-auto'>
+                <h1 className="text-xl md:text-4xl font-bold text-[#E72726]">Oops! This page took a break for popcorn.</h1>
+                <NavLink to={'/'} className='btn text-white font-bold text-sm md:text-2xl mt-10 bg-gradient-to-r from-[#E72726] to-black'>Let's Go Home</NavLink>
+            </div>
         </div>
     );
 };
